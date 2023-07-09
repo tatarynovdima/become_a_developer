c4895ccb22e309ce7a139595385349d163ee5520
tatarynovdima <dmytro.course@gmail.com>
2023-07-09 18:42:03 +0200

    Initial commit

---
+++ src/main/java/com/example/become_a_developer/BecomeADeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.example.become_a_developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecomeADeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BecomeADeveloperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/become_a_developer/BecomeADeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.become_a_developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecomeADeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
