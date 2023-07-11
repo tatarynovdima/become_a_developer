4e427e5daab6b8e652d305e39713f2310fb1e647
tatarynovdima <dmytro.course@gmail.com>
2023-07-11 22:53:12 +0200

    refactor

--- src/main/java/com/example/become_a_developer/controller/TextAnalyzerController.java
+++ src/main/java/com/example/become_a_developer/controller/TextAnalyzerController.java
@@ -1,5 +1,6 @@
 package com.example.become_a_developer.controller;
 
+import com.example.become_a_developer.repository.TextAnalyzerRepositoryImpl;
 import com.example.become_a_developer.service.TextAnalyzerServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,12 +8,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api/analyzer")
 public class TextAnalyzerController {
     private final TextAnalyzerServiceImpl textAnalyzerService;
+    private final TextAnalyzerRepositoryImpl textAnalyzerRepository;
 
-    public TextAnalyzerController(TextAnalyzerServiceImpl textAnalyzerService) {
+    public TextAnalyzerController(TextAnalyzerServiceImpl textAnalyzerService, TextAnalyzerRepositoryImpl textAnalyzerRepository) {
         this.textAnalyzerService = textAnalyzerService;
+        this.textAnalyzerRepository = textAnalyzerRepository;
     }
-    @GetMapping
+
+
+    @GetMapping("")
     public String getText(){
         return textAnalyzerService.findUniqueCharacter();
+    }
+
+    @PostMapping("/analyzessss")
+    public String analyzeText(@RequestBody String text) {
+        textAnalyzerRepository.setText(text);
+        return "Анализ текста: " + text;
     }
 }
--- src/main/java/com/example/become_a_developer/repository/TextAnalyzerRepositoryImpl.java
+++ src/main/java/com/example/become_a_developer/repository/TextAnalyzerRepositoryImpl.java
@@ -3,21 +3,18 @@ package com.example.become_a_developer.repository;
 
 import org.springframework.stereotype.Repository;
 
+import org.springframework.stereotype.Repository;
+
 @Repository
 public class TextAnalyzerRepositoryImpl implements TextAnalyzerRepository {
+    private String text;
+
     @Override
     public String getText() {
-        return MockText();
+        return text;
     }
-    private String MockText(){
-        return "The Tao gave birth to machine language.  Machine language gave birth\n" +
-                "to the assembler.\n" +
-                "The assembler gave birth to the compiler.  Now there are ten thousand\n" +
-                "languages.\n" +
-                "Each language has its purpose, however humble.  Each language\n" +
-                "expresses the Yin and Yang of software.  Each language has its place within\n" +
-                "the Tao.\n" +
-                "But do not program in COBOL if you can avoid it.\n" +
-                "        -- Geoffrey James, \"The Tao of Programming\"";
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
