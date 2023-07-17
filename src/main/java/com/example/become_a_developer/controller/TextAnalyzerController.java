79610658875c4703ccf0b98738da312a7e3eb12c
tatarynovdima <dmytro.course@gmail.com>
2023-07-17 22:55:23 +0200

    work commit

--- src/main/java/com/example/become_a_developer/controller/TextAnalyzerController.java
+++ src/main/java/com/example/become_a_developer/controller/TextAnalyzerController.java
@@ -2,10 +2,14 @@ package com.example.become_a_developer.controller;
 
 import com.example.become_a_developer.repository.TextAnalyzerRepositoryImpl;
 import com.example.become_a_developer.service.TextAnalyzerServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping(path = "/api/analyzer")
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
 public class TextAnalyzerController {
     private final TextAnalyzerServiceImpl textAnalyzerService;
     private final TextAnalyzerRepositoryImpl textAnalyzerRepository;
@@ -14,16 +18,17 @@ public class TextAnalyzerController {
         this.textAnalyzerService = textAnalyzerService;
         this.textAnalyzerRepository = textAnalyzerRepository;
     }
-
-
-    @GetMapping("")
-    public String getText(){
-        return textAnalyzerService.findUniqueCharacter();
+    @GetMapping("/analyze")
+    public String showAnalyzePage() {
+        return "index";
     }
 
-    @PostMapping("/analyzessss")
-    public String analyzeText(@RequestBody String text) {
+    @PostMapping("/analyze")
+    @ResponseBody
+    public ResponseEntity<String> analyzeText(@RequestBody Map<String, String> requestBody) {
+        String text = requestBody.get("text");
         textAnalyzerRepository.setText(text);
-        return "Анализ текста: " + text;
+        String uniqueCharacter = textAnalyzerService.findUniqueCharacter();
+        return ResponseEntity.ok(uniqueCharacter != null ? uniqueCharacter : "No unique character found");
     }
 }
